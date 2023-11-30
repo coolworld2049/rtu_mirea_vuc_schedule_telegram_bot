2f283559f2755ba93c501178db59bb012b1c456f
coolworld2049 <coolworld2049@gmail.com>
2023-11-30 22:18:53 +0300

    1.0.4 session_schedule

--- telegram_bot/handlers/schedule/base.py
+++ telegram_bot/handlers/schedule/base.py
@@ -1,7 +1,12 @@
 from contextlib import suppress
 
 from aiogram.types import CallbackQuery, Message
-from rtu_mirea_vuc_schedule_client import ApiClient, ScheduleApi, WorkbookApi
+from rtu_mirea_vuc_schedule_client import (
+    ApiClient,
+    ScheduleApi,
+    WeekScheduleResult,
+    WorkbookApi,
+)
 
 from telegram_bot.handlers.utils import del_prev_message
 from telegram_bot.schemas import UserSettingsBase
@@ -11,7 +16,7 @@ from telegram_bot.template_engine import render_template
 
 async def common_schedule_logic(
     api_client,
-    data,
+    data: list[WeekScheduleResult],
     telegram_obj: Message | CallbackQuery,
     user_settings: UserSettingsBase,
 ):
@@ -24,12 +29,15 @@ async def common_schedule_logic(
     ).get_workbook_relevance_api_v1_workbook_relevance_get(
         course=user_settings.course,
     )
-    text = render_template(
-        "week_schedule.html",
-        data=data,
-        relevance_date=relevance_date.split("T")[0],
-        user_settings=user_settings,
-    )
+    text = ""
+    for d in data:
+        _text = render_template(
+            "week_schedule.html",
+            data=d,
+            relevance_date=relevance_date.split("T")[0],
+            user_settings=user_settings,
+        )
+        text += f"{_text}\n\n"
     return text
 
 
@@ -37,12 +45,14 @@ async def get_study_day_schedule(
     telegram_obj: Message | CallbackQuery, user_settings: UserSettingsBase, **kwargs
 ):
     async with ApiClient(settings.schedule_api_configuration) as api_client:
-        data = await ScheduleApi(api_client).get_week_schedule_api_v1_schedule_week_get(
+        data: list[WeekScheduleResult] = await ScheduleApi(
+            api_client,
+        ).get_week_schedule_api_v1_schedule_week_get(
             **user_settings.model_dump(exclude_none=True), **kwargs
         )
         return await common_schedule_logic(
             api_client=api_client,
-            data=data[0],
+            data=data,
             telegram_obj=telegram_obj,
             user_settings=user_settings,
         )
@@ -56,10 +66,13 @@ async def get_session_schedule(
             **user_settings.model_dump(exclude_none=True), **kwargs
         )
         data = list(filter(lambda c: len(c.model_dump().get("schedule")) > 0, data))
-        data = list(map(lambda c: c.schedule, data))
+        map_data = list(map(lambda c: c.schedule, data))
+        map_data: list[WeekScheduleResult] = [
+            x[0] for x in list(map(lambda c: c.schedule, data))
+        ]
         return await common_schedule_logic(
             api_client=api_client,
-            data=data[0][0],
+            data=map_data,
             telegram_obj=telegram_obj,
             user_settings=user_settings,
         )
--- telegram_bot/handlers/session/main.py
+++ telegram_bot/handlers/session/main.py
@@ -4,12 +4,13 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from telegram_bot.handlers.schedule.base import get_session_schedule
 from telegram_bot.handlers.user_settings.base import get_user_settings
-from telegram_bot.handlers.utils import del_prev_message
+from telegram_bot.handlers.utils import del_prev_message, process_handler_error
 
 router = Router(name=__file__)
 
 
 @router.message(Command("session"))
+@process_handler_error
 async def session_schedule(message: types.Message, session: AsyncSession):
     await del_prev_message(message)
 
@@ -17,14 +18,8 @@ async def session_schedule(message: types.Message, session: AsyncSession):
     text = await get_session_schedule(
         message,
         user_settings=user_settings,
-        where="зачет",
+        where="зачет экзамен",
     )
-    exams_text = await get_session_schedule(
-        message,
-        user_settings=user_settings,
-        where="экзамен",
-    )
-    text = f"<em>Зачеты</em>\n\n{text}\n\n<em>Экзамены</em>\n\n{exams_text}"
     if not text:
         return
     await message.answer(text=text)
